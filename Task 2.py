1fe8b4d28159047b38b6525b1b90dfbb2c91769e
Zishan Ansari <108237273+Peterpaul0025@users.noreply.github.com>
2025-10-27 08:39:48 +0530

    Add files via upload

---
+++ Task 2.py
@@ -0,0 +1,6 @@
+# Program to calculate the sum of numbers from 1 to 50
+# Initialize a variable to store the sum
+total = 0
+for num in range(1, 51):
+    total += num  # Add each number to total
+print("The sum of numbers from 1 to 50 is:", total)
